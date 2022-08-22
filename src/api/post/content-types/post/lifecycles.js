d0153cd875345cb25c745276fa560e75cad4088a
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-08-22 20:47:42 -0300

    Update lifecycles.js

--- src/api/post/content-types/post/lifecycles.js
+++ src/api/post/content-types/post/lifecycles.js
@@ -3,13 +3,11 @@
 const axios = require('axios');
 
 module.exports = {
-  lifecycles: {
-    async afterCreate(result, data) {
-      axios.post('https://api.netlify.com/build_hooks/6302e342eb51c70455d0604f');
-    },
+  async afterCreate(result, data) {
+    axios.post('https://api.netlify.com/build_hooks/6302e342eb51c70455d0604f');
+  },
 
-    async afterUpdate(result, params, data) {
-      axios.post('https://api.netlify.com/build_hooks/6302e342eb51c70455d0604f');
-    },
+  async afterUpdate(result, params, data) {
+    axios.post('https://api.netlify.com/build_hooks/6302e342eb51c70455d0604f');
   },
 }
